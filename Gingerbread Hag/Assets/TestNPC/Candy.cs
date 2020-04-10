1af590b01db50e0a2abc1296309b38e96b805640
caseyot6 <55035980+caseyot6@users.noreply.github.com>
2020-04-10 14:54:48 -0400

    Children movement demo
    Children Exist in test file with example movement

---
+++ Gingerbread Hag/Assets/TestNPC/Candy.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Candy : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public bool visited = false;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    
+}
