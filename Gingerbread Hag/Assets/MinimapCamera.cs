35f3ad87e41374b25c2d6ef0277f07124c22a84d
caseyot6 <55035980+caseyot6@users.noreply.github.com>
2020-04-23 19:29:15 -0400

    ui change
    ui change

---
+++ Gingerbread Hag/Assets/MinimapCamera.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MinimapCamera : MonoBehaviour
+{
+    // Start is called before the first frame update
+    GameObject player;
+    void Start()
+    {
+        player = GameObject.FindWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        transform.position = new Vector3(player.transform.position.x, this.transform.position.y, player.transform.position.z);
+    }
+}
