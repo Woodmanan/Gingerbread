0ecab206a28bf58907c85299fd10a7111223c66d
Yumi S <ysato@andrew.cmu.edu>
2020-04-30 18:56:41 -0400

    dialogue manager stuff in, might need to tweak some things to fit this game

---
+++ Gingerbread Hag/Assets/Scripts/DialogueManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueManager : MonoBehaviour
+{
+    public Text dialogueText;
+    private Queue<string> sentences;
+
+    void Start()
+    {
+        sentences = new Queue<string>();
+    }
+
+    public void StartDialogue (Dialogue dialogue)
+    {
+        Debug.Log("Starting");
+        sentences.Clear();
+        foreach (string sentence in dialogue.sentences)
+        {
+            sentences.Enqueue(sentence);
+        }
+
+        DisplayNextSentence();
+    }
+
+    public void DisplayNextSentence()
+    {
+        if (sentences.Count == 0) {
+            Debug.Log("End");
+            dialogueText.text = "";
+            return;
+        }
+        string sentence = sentences.Dequeue();
+        dialogueText.text = sentence;
+    }
+}
---
+++ Gingerbread Hag/Assets/Scripts/DialogueTrigger.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueTrigger : MonoBehaviour
+{
+    public Dialogue dialogue;
+    public GameObject[] nexts;
+    public GameObject[] others;
+
+    public Text text;
+
+    int c = 0;
+
+    void OnMouseDown()
+    {
+        DialogueManager dialogueManager = FindObjectOfType<DialogueManager>();
+        Debug.Log(c);
+        if (c == dialogue.sentences.Length)
+        {
+            dialogueManager.DisplayNextSentence();
+            gameObject.SetActive(false);
+            foreach (GameObject next in nexts)
+            {
+                if (next != null)
+                {
+                    next.SetActive(true);
+                }
+
+            }
+
+            foreach (GameObject other in others)
+            {
+                if (other != null)
+                {
+                    other.SetActive(false);
+                }
+
+            }
+        }
+
+        else if (c == 0)
+        {
+            dialogueManager.StartDialogue(dialogue);
+            c++;
+        }
+
+
+        else
+        {
+            dialogueManager.DisplayNextSentence();
+            c++;
+        }
+    }
+
+}
